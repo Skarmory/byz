e712bb532d020a744addc379ea20533f2afdffd0
Song Taezhou <song.taezhou@gmail.com>
2022-02-13 11:30:30 +0000

    Refactor ui_map

--- include/ui/ui_map.h
+++ include/ui/ui_map.h
@@ -1,20 +1,9 @@
 #ifndef BYZ_UI_MAP_H
 #define BYZ_UI_MAP_H
 
-#include "ui/ui_box.h"
-
+struct camera;
 struct Map;
 
-struct UIMap
-{
-    struct UIBox ui_box;
-    struct Map*  map;
-};
-
-struct UIMap* ui_map_new(int x, int y, int w, int h, struct Map* map);
-void          ui_map_free(struct UIMap* ui_map);
-
-void          ui_map_draw(struct UIMap* ui_map);
-void          ui_map_set_map(struct UIMap* ui_map, struct Map* map);
+void ui_map_draw(struct Map* map, struct camera* camera);
 
 #endif
