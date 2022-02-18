e3c1ae0ac4bf8fa5f9144ed5ff2e85131c4e6f4f
Song Taezhou <song.taezhou@gmail.com>
2022-02-18 10:22:13 +0000

    Fix map gen utils using old pathing data

--- src/game/map_gen_utils.c
+++ src/game/map_gen_utils.c
@@ -1,19 +1,20 @@
 #include "game/map_gen_utils.h"
 
 #include "game/map_location.h"
+#include "game/pathing_node.h"
 
 #include <stddef.h>
 
 void map_gen_util_make_corridor(struct MapLocation* loc)
 {
     loc->symbol.sym = '#';
-    loc->pathing_flags = PATHING_GROUND;
+    loc->pathing->pathing_flags = PATHING_GROUND;
     loc->feature = NULL;
 }
 
 void map_gen_util_make_floor(struct MapLocation* loc)
 {
     loc->symbol.sym = '.';
-    loc->pathing_flags = PATHING_GROUND;
+    loc->pathing->pathing_flags = PATHING_GROUND;
     loc->feature = NULL;
 }
