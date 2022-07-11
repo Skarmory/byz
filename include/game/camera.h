072f24b6fc1313c5a641eb086cd22b75775f3efe
Song Taezhou <song.taezhou@gmail.com>
2022-07-11 15:20:40 +0100

    Added camera relative to world function

--- include/game/camera.h
+++ include/game/camera.h
@@ -17,6 +17,7 @@ bool camera_in_bounds(struct camera* camera, int x, int y);
 
 bool camera_world_to_screen(struct camera* camera, int world_x, int world_y, int* out_screen_x, int* out_screen_y);
 bool camera_screen_to_world(struct camera* camera, int screen_x, int screen_y, int* out_world_x, int* out_world_y);
+bool camera_relative_to_world(struct camera* camera, int rx, int ry, int* out_world_x, int* out_world_y);
 
 extern struct camera* g_camera;
 
