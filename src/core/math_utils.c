5acce4d03e3d3ca3c40554e77857625fc4c80fc2
Song Taezhou <song.taezhou@gmail.com>
2022-07-11 15:13:26 +0100

    Added change basis function

--- src/core/math_utils.c
+++ src/core/math_utils.c
@@ -23,3 +23,14 @@ float map_range(float value, float old_low, float old_high, float new_low, float
 
     return (f * (new_high - new_low)) + new_low;
 }
+
+// Transform (x, y) from coordinate system with origin (old_ox, old_oy) to new origin (new_ox, new_oy)
+// e.g. old = (4, 7)
+//      new = (8, 3)
+//      in  = (2, 2)
+//      output = (2 - 4 + 8, 2 - 7 + 3) = (6, -2)
+void math_change_basis(const int x, const int y, const int old_ox, const int old_oy, const int new_ox, const int new_oy, int* x_out, int* y_out)
+{
+    *x_out = (x - old_ox) + new_ox;
+    *y_out = (y - old_oy) + new_oy;
+}
