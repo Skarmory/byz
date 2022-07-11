1ebf487bf670a548f48d8a441fb7812a81a47c90
Song Taezhou <song.taezhou@gmail.com>
2022-07-11 15:15:45 +0100

    Moved pathing evaluation out of pathing.c
    Return a list of path nodes

--- include/game/pathing.h
+++ include/game/pathing.h
@@ -1,23 +1,13 @@
-#ifndef BYZ_PATHING_F_H
-#define BYZ_PATHING_F_H
+#ifndef BYZ_PATHING_H
+#define BYZ_PATHING_H
 
 #include "game/pathing_flags.h"
+#include "game/pathing_eval.h"
 
 struct CONNECTIVITY_NODE;
 struct List;
-struct Map;
 struct PATHING_GRID;
 
-typedef float(*evaluation_func)(struct CONNECTIVITY_NODE* node, struct CONNECTIVITY_NODE* dest, PathingFlags pather_flags);
-
-float creature_manhatten_evaluation(struct CONNECTIVITY_NODE* node, struct CONNECTIVITY_NODE* dest, PathingFlags pather_flags);
-
-void pathing_find_path(
-        struct CONNECTIVITY_NODE* start,
-        struct CONNECTIVITY_NODE* end,
-        PathingFlags pather_flags,
-        evaluation_func eval_f,
-        struct List* out_path
-    );
+void pathing_find_path(struct CONNECTIVITY_NODE* start, struct CONNECTIVITY_NODE* end, PathingFlags pather_flags, evaluation_func eval_f, struct List* out_path);
 
 #endif
--- src/game/pathing.c
+++ src/game/pathing.c
@@ -8,7 +8,6 @@
 #include "game/movement.h"
 
 #include <stddef.h>
-#include <math.h>
 
 // TODO: Implement a sorted linked list?
 
@@ -195,29 +194,6 @@ static void _log_path(struct CONNECTIVITY_NODE* node)
     log_pop_indent();
 }
 #endif
-
-float creature_manhatten_evaluation(struct CONNECTIVITY_NODE* node, struct CONNECTIVITY_NODE* dest, PathingFlags pather_flags)
-{
-    // Special handling for the destination
-    if(node->x == dest->x && node->y == dest->y)
-    {
-        return -INFINITY;
-    }
-
-    // Check to see whether the pather can use this location
-    if(!pathing_can_path(node->pathing_flags, pather_flags))
-    {
-        // Cannot path to this node, so make it ridicuously high evaluation
-        return INFINITY;
-    }
-
-    // Use distance squared to avoid sqrt
-    float _x = dest->x - node->x;
-    float _y = dest->y - node->y;
-    float dist2 = _x * _x + _y * _y;
-
-    return dist2 + node->weight;
-}
 
 void pathing_find_path(struct CONNECTIVITY_NODE* restrict start, struct CONNECTIVITY_NODE* restrict end, PathingFlags pather_flags, evaluation_func eval_f, struct List* out_path)
 {
