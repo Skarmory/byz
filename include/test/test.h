5608d3d283191f05ebf39fa91e03ba8c69dcc9a1
Song Taezhou <song.taezhou@gmail.com>
2022-07-24 11:43:01 +0100

    Updated test functions

--- include/test/test.h
+++ include/test/test.h
@@ -6,7 +6,12 @@
 #define TEST_CASE_FAIL false
 #define TEST_CASE_SUCCESS true
 
-bool test_assert_equal_char_buffer(const char* lhs, const char* rhs);
-bool test_assert_equal_int(const int lhs, const int rhs);
+typedef bool(*test_func)(void);
+
+bool test_assert_equal_char_buffer(const char* expect, const char* actual);
+bool test_assert_equal_int(const int expect, const int actual);
+
+void test_run_test_block(const char* block_name, test_func func);
+bool test_run_test(const char* test_name, test_func func);
 
 #endif
