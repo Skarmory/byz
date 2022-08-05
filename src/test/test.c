6550802605ae7c74e57ec1659e355f6d96d56341
Song Taezhou <song.taezhou@gmail.com>
2022-08-05 15:07:38 +0100

    Fix test log push/pop indents to use flags

--- src/test/test.c
+++ src/test/test.c
@@ -6,7 +6,7 @@
 
 bool test_assert_equal_char_buffer(const char* expect, const char* actual)
 {
-    log_push_indent();
+    log_push_indent(LOG_STDOUT | LOG_TEST);
 
     log_msg(LOG_STDOUT | LOG_TEST, "Test char buffers.");
     log_format_msg(LOG_STDOUT | LOG_TEST, "Expect: %s", expect);
@@ -30,11 +30,11 @@ bool test_assert_equal_char_buffer(const char* expect, const char* actual)
         }
     }
 
-    log_pop_indent();
+    log_pop_indent(LOG_STDOUT | LOG_TEST);
     return TEST_CASE_SUCCESS;
 
 test_assert_equal_char_buffer_fail:
-    log_pop_indent();
+    log_pop_indent(LOG_STDOUT | LOG_TEST);
     return TEST_CASE_FAIL;
 }
 
@@ -43,25 +43,41 @@ bool test_assert_equal_int(const int expect, const int actual)
     return expect == actual;
 }
 
+bool test_assert_equal_bool(const bool expect, const bool actual)
+{
+    return expect == actual;
+}
+
+bool test_assert_not_null(void* value)
+{
+    return value != NULL;
+}
+
+bool test_assert_null(void* value)
+{
+    return value == NULL;
+}
+
 void test_run_test_block(const char* block_name, test_func func)
 {
     log_msg(LOG_STDOUT | LOG_TEST, "");
     log_format_msg(LOG_STDOUT | LOG_TEST, "Test Block: %s", block_name);
-    log_push_indent();
+    log_push_indent(LOG_STDOUT | LOG_TEST);
 
     bool success = func();
 
     log_msg(LOG_STDOUT | LOG_TEST, success ? "SUCCESS" : "FAILED");
+    log_pop_indent(LOG_STDOUT | LOG_TEST);
 }
 
 bool test_run_test(const char* test_name, test_func func)
 {
     log_format_msg(LOG_STDOUT | LOG_TEST, "Test: %s", test_name);
-    log_push_indent();
+    log_push_indent(LOG_STDOUT | LOG_TEST);
 
     bool success = func();
 
-    log_pop_indent();
+    log_pop_indent(LOG_STDOUT | LOG_TEST);
     log_msg(LOG_STDOUT | LOG_TEST, success ? "SUCCESS" : "FAILED");
 
     return success;
