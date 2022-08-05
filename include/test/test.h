4e46ebfabfa8cb7ede8355beb32cb4c3623dfad9
Song Taezhou <song.taezhou@gmail.com>
2022-08-05 15:09:51 +0100

    Added missing test header decls

--- include/test/test.h
+++ include/test/test.h
@@ -10,6 +10,9 @@ typedef bool(*test_func)(void);
 
 bool test_assert_equal_char_buffer(const char* expect, const char* actual);
 bool test_assert_equal_int(const int expect, const int actual);
+bool test_assert_equal_bool(const bool expect, const bool actual);
+bool test_assert_not_null(void* value);
+bool test_assert_null(void* value);
 
 void test_run_test_block(const char* block_name, test_func func);
 bool test_run_test(const char* test_name, test_func func);
